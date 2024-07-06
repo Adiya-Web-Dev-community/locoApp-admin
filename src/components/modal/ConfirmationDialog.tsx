33cea0d1c068dcf63259abaa97d21eea6863a970
Mansoor Khan <mansoorKh1098@gmail.com>
2024-07-06 19:05:50 +0530

    created sponsor profile loading ui and integrated company product api creat, get, delet, post

--- src/components/modal/ConfirmationDialog.tsx
+++ src/components/modal/ConfirmationDialog.tsx
@@ -13,22 +13,22 @@ const ConfirmationDialog = ({ show, onClose, onConfirm }) => {
       onClick={onClose}
     >
       <div
-        className="p-6 bg-[#252525] text-[#DEE1E2] rounded-md  cursor-default"
+        className="p-6 bg-white rounded-md cursor-default"
         onClick={handlingPropogation}
       >
         <h2 className="mb-4 text-lg font-semibold">
           Are you sure you want to move to this website?
         </h2>
-        <div className="flex justify-end space-x-4">
+        <div className="flex justify-end space-x-4 text-white">
           <button
             onClick={onClose}
-            className="px-4 py-2 font-semibold text-gray-800 bg-gray-400 rounded-md hover:bg-gray-400"
+            className="px-4 py-2 font-semibold text-black bg-gray-200 rounded-md hover:bg-gray-300"
           >
             Cancel
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 font-semibold rounded-md bg-rose-800 hover:bg-rose-700"
+            className="px-4 py-2 font-semibold rounded-md bg-rose-600 hover:bg-rose-700"
           >
             Confirm
           </button>
